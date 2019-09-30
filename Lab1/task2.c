f60164942fc289c83990b60ca3ef4fc460f149cc
Ethan Paek <ethanpaek17@gmail.com>
2019-09-29 22:07:17 -0700

    Second Task is Finished

--- Lab1/task2.c
+++ Lab1/task2.c
@@ -5,9 +5,9 @@
 int main(){
   printf("The OG process is: %d\n", getpid());//Test the program to find the original process ID
   for(int i = 0; i < 6; i++){//we must only do exactly 6 child processes
-    if(fork() != 0){//fork off of child process, test if it's the parent process
-      waitpid(-1, NULL, 0);//wait for child process to finish
-      return 0;//return 0 will return flow of control to the method caller
+    if(fork() != 0){//fork off of child process, test if it's the parent process; if it doesn't return to the newly created child process
+      waitpid(-1, NULL, 0);//wait for any child process to finish
+      break;//necessary so that we don't keep forking off of the child processes
     }
     else{
       printf("Child is %d, and its Father (or Mother?) is %d\n", getpid(), getppid());
